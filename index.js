76357043c449739855e42190083f00d65cd4489a
Abdulhadi5692HDI <87948364+Abdulhadi5692HDI@users.noreply.github.com>
2022-06-17 22:36:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("Passwords Generator!");
+
+// For use in other js apps
+function RandomGenerate(){
+	const pass1 = Math.random();
+	console.log("Password: ", pass1);
+}
+console.log('Password: ', Math.random());
+console.log('Password: ', Math.random());
+console.log('Password: ', Math.random());
+console.log('Password: ', Math.random());
+console.log('Password: ', Math.random());
+console.log('Password: ', Math.random());
+console.log('Password: ', Math.random());
+console.log('Password: ', Math.random());
+console.log('Password: ', Math.random());
+console.log('Password: ', Math.random());
+console.log('Password: ', Math.random());
+console.log('Password: ', Math.random());
+console.log('Password: ', Math.random());
