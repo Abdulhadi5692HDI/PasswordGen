191b4f8b66b41bdd947f1928c96688bfa0c32717
Abdulhadi5692HDI <87948364+Abdulhadi5692HDI@users.noreply.github.com>
2022-06-18 12:11:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 console.log("Passwords Generator!");
-
+passgen.Gen13Pass();
 // For use in other js apps
 passgen.RandomGenerate = function(){
 	const pass1 = Math.random();
 	console.log("Password: ", pass1);
 }
+passgen.Gen13Pass = function(){
 console.log('Password: ', Math.random());
 console.log('Password: ', Math.random());
 console.log('Password: ', Math.random());
@@ -18,3 +19,4 @@ console.log('Password: ', Math.random());
 console.log('Password: ', Math.random());
 console.log('Password: ', Math.random());
 console.log('Password: ', Math.random());
+}
